4b56d2364310e0411be72a74c445c68380fbc02d
aiman <abduldanmaihaja@gmail.com>
2022-08-04 02:32:32 +0100

    finishCRUD operation for TOUR route

--- routes/tour-routes.js
+++ routes/tour-routes.js
@@ -5,13 +5,11 @@ const {
   getTour,
   deleteTour,
   updateTour,
-  checkID,
-  checkBody,
 } = require('../controllers/tours-controller');
-const router = express.Router();
 
-router.param('id', checkID);
-router.route('/').get(getAllTours).post(checkBody, createTour);
+const router = express.Router();
+// router.param('id', checkID);
+router.route('/').get(getAllTours).post(createTour);
 
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 module.exports = router;
